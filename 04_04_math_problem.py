a20f5f3fa4b6e18cd8368dd07d1a53f460bdaa9d
javawillch <javawillch@gmail.com>
2021-05-13 23:02:19 +0800

    add method 2 for 04_04

--- 04_04_math_problem.py
+++ 04_04_math_problem.py
@@ -3,7 +3,6 @@
 # knife = 3
 # current_num = 1
 
-
 # while current_num < knife:
 
 def cutbar(m, n, current):
@@ -16,3 +15,15 @@ def cutbar(m, n, current):
     
 
 print(cutbar(3, 20, 1))
+
+def cuttbar(knife, total_length):
+    count = 0 
+    current_num = 1
+    while total_length > current_num:
+        current_num += current_num if current_num < knife else knife
+        count = count + 1
+    return count
+
+
+ 
+print(cuttbar(3, 20))   
