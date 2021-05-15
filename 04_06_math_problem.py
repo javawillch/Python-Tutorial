cf818df3109166ca4117649834804c9efde2a09e
javawillch <javawillch@gmail.com>
2021-05-16 00:26:42 +0800

    04_06 slove

--- 04_06_math_problem.py
+++ 04_06_math_problem.py
@@ -1,7 +1,21 @@
+def collatz(init, curr, isFirst):
+    if curr == 1:
+        return False
+    elif curr == init:
+        return True
+    else:
+        if isFirst == True:
+            curr = init * 3 + 1
+        if curr % 2 == 0:
+            return collatz(init, curr/2, False)
+        else:
+            return collatz(init, curr*3 + 1, False)
 
 def inLoopCount(target):
+    result = 0
     for i in range(2, target, 2):
-        print(i)
+        if collatz(i, 0, True) == True:
+            result += 1
+    return result
 
-
-inLoopCount(100)
+print(inLoopCount(10000))
